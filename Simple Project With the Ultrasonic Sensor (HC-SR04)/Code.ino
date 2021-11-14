d5c7d477750ed0e5d2d4046761129a097d208f69
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-11-14 21:04:41 +0600

    Project - 01

---
+++ Simple Project With the Ultrasonic Sensor (HC-SR04)/Code.ino
@@ -0,0 +1,36 @@
+
+#define trigPin 3
+#define echoPin 2
+#define led 13
+#define led_2 12
+
+void setup(){
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led_2, OUTPUT);
+}
+
+void loop(){ 
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+
+  if (distance < 20){
+    digitalWrite(led,HIGH);
+    digitalWrite(led_2,LOW);
+    }
+  else {
+    digitalWrite(led,LOW);
+    digitalWrite(led_2,HIGH);
+    }
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+}
