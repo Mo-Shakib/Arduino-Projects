9691f8b4e56c6671c4d20f78923a4657cfe82abb
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-11-20 04:19:21 +0600

    ✨

--- Ultrasonic Sensor/Code.ino
+++ Ultrasonic Sensor/Code.ino
@@ -4,15 +4,17 @@
 #define led 13
 #define led_2 12
 
-void setup(){
-  Serial.begin (9600);
+void setup()
+{
+  Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(led, OUTPUT);
   pinMode(led_2, OUTPUT);
 }
 
-void loop(){ 
+void loop()
+{
   long duration, distance;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -20,16 +22,18 @@ void loop(){
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
+  distance = (duration / 2) / 29.1;
 
-  if (distance < 20){
-    digitalWrite(led,HIGH);
-    digitalWrite(led_2,LOW);
-    }
-  else {
-    digitalWrite(led,LOW);
-    digitalWrite(led_2,HIGH);
-    }
+  if (distance < 20)
+  {
+    digitalWrite(led, HIGH);
+    digitalWrite(led_2, LOW);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+    digitalWrite(led_2, HIGH);
+  }
   Serial.print(distance);
   Serial.println(" cm");
   delay(500);
